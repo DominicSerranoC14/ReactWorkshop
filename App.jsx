c5eca27b2230ee282226126959b891fff6c2b421
Dominic Serrano <dmsrojo@gmail.com>
2017-01-11 16:48:58 -0600

    Styled App component with React styling method

--- App.jsx
+++ App.jsx
@@ -3,22 +3,26 @@
 import React from 'react';
 
 class App extends React.Component {
-   render() {
-      return (
-        // to render multiple elements they must be
-        // wrapped in a container element
-         <div>
-            <h1>Hello World!!!</h1>
-            <h3>Hello World!!!</h3>
-            <p>Random number {Math.floor(Math.random() * 10)}</p>
-            <p>-------------</p>
-            <p>Using a ternary: (cannot use if statements):</p>
-            <p>Is the current date an odd number? {(Date().slice(8, 10) % 2) ? <b>Yes</b>: <b>No</b>}</p>
-            <p>Is the current date an odd number? {(Date().slice(8, 10) % 1) ? <b>Yes</b>: <b>No</b>}</p>
-            <p>-------------</p>
-         </div>
-      );
-   }
+
+  render() {
+    // How to style with React. Using inline method
+    const mainStyle = { border: 'black solid 2px', textAlign: 'center' };
+
+    return (
+      // to render multiple elements they must be
+      // wrapped in a container element
+       <div style={mainStyle}>
+          <h1>Hello World!!!</h1>
+          <h3>Hello World!!!</h3>
+          <p>Random number {Math.floor(Math.random() * 10)}</p>
+          <p>-------------</p>
+          <p>Using a ternary: (cannot use if statements):</p>
+          <p>Is the current date an odd number? {(Date().slice(8, 10) % 2) ? <b>Yes</b>: <b>No</b>}</p>
+          <p>Is the current date an odd number? {(Date().slice(8, 10) % 1) ? <b>Yes</b>: <b>No</b>}</p>
+          <p>-------------</p>
+       </div>
+    );
+  }
 }
 
 export default App;
