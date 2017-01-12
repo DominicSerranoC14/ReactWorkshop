8a7cdc177f47115a9ff1ff50d360e6480c3ce34f
Dominic Serrano <dmsrojo@gmail.com>
2017-01-12 09:21:58 -0600

    Using a components state and rendering multiple elements from state data

--- main.js
+++ main.js
@@ -2,11 +2,15 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx';
-import MultiApp from './MultiApp.jsx';
+import App from './jsx/App.jsx';
+import MultiApp from './jsx/MultiApp.jsx';
+import StateApp from './jsx/StateApp.jsx';
 
 // Render App.jsx
 // ReactDOM.render(<App />, document.getElementById('app'));
 
 // Render MultiApp.jsx
-ReactDOM.render(<MultiApp />, document.getElementById('app'));
+// ReactDOM.render(<MultiApp />, document.getElementById('app'));
+
+// Render StateApp.jsx
+ReactDOM.render(<StateApp />, document.getElementById('app'));
