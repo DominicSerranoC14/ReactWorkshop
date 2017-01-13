93245009cecc72311b9bddc4418c72efe667af7e
Dominic Serrano <dmsrojo@gmail.com>
2017-01-13 15:01:18 -0600

    Passed function from parent component to child component

--- jsx/KeyApp.jsx
+++ jsx/KeyApp.jsx
@@ -11,15 +11,23 @@ class KeyApp extends React.Component {
         { text: "Button 1" },
         { text: "Button 2" },
         { text: "Button 3" }
-      ]
+      ],
+      h1Data: ''
     };
 
+    this.changeText = this.changeText.bind(this);
+
+  };
+
+  changeText(e) {
+    this.setState({ h1Data: e.target.value })
   };
 
   render() {
     return (
       <div>
-        {this.state.data.map((each, i) => <Content key={i} propValue={each.text} />)}
+        {this.state.data.map((each, i) => <Content key={i} propValue={each.text} changeTextProp={this.changeText} />)}
+        <h1>{this.state.h1Data}</h1>
       </div>
     )
   };
@@ -31,7 +39,8 @@ class Content extends React.Component {
   render() {
     return (
       <div>
-        <input type="button" value={this.props.propValue} />
+        <input type="button" value={this.props.propValue}
+        onClick={this.props.changeTextProp} />
       </div>
     )
   }
