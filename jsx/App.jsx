d9c0a0c9fa7befea28e33786853c35e6b4ac38fe
Dominic Serrano <dmsrojo@gmail.com>
2017-01-26 16:55:04 -0600

    Committing simple change

--- jsx/App.jsx
+++ jsx/App.jsx
@@ -18,7 +18,7 @@ class App extends React.Component {
           <p>Random number {Math.floor(Math.random() * 10)}</p>
           <p>-------------</p>
           <p>Using a ternary: (cannot use if statements):</p>
-          <p>Is the current date an odd number? {(Date().slice(8, 10) % 2) ? <b>Yes</b>: <b>No</b>}</p>
+          <p>Is the current date an even number? {(Date().slice(8, 10) % 2) ? <b>Yes</b>: <b>No</b>}</p>
           <p>Is the current date an odd number? {(Date().slice(8, 10) % 1) ? <b>Yes</b>: <b>No</b>}</p>
           <p>-------------</p>
        </div>
